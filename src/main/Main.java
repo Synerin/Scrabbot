d02e268ee65d397d14235944f4d68e1af64726df
Jason <jasondavis929@gmail.com>
2021-06-25 18:44:20 -0400

    begin building GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,23 +7,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Scrabble scrabble = new Scrabble();
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Scrabbot");
         GridPane scrabbleBoard = new GridPane();
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        scrabble.setBoard();
+
+        Button btnBest = new Button();
+        btnBest.setText("Find Best Word");
+        btnBest.setOnAction(event -> System.out.println("To be implemented"));
+
+        for(int r = 0; r < 15; r++) {
+            for(int c = 0; c < 15; c++) {
+                TextField space = new TextField();
+                space.setText(String.valueOf(scrabble.board[r][c]));
+                scrabbleBoard.add(space, c, r);
+            }
+        }
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 275));
+        root.getChildren().add(scrabbleBoard);
+        primaryStage.setScene(new Scene(root, 720, 720));
         primaryStage.show();
     }
 
